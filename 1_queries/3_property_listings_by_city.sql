94053f1d2f3162d8dbe9f5d0550ad51039d8f463
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-10-13 16:58:57 -0600

    Completed 1_queries

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,7 @@
+SELECT properties.*, AVG(rating) AS average_rating
+FROM properties JOIN property_reviews ON property_id = properties.id
+WHERE city LIKE '%Vancouver%'
+GROUP BY properties.id
+HAVING AVG(rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,7 @@
+SELECT properties.*, reservations.*, AVG(rating) AS average_rating
+FROM reservations JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE reservations.guest_id = 1 AND end_date < now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY start_date
+LIMIT 10;
