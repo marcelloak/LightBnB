064d41b463fed444a9c79bb120a16745e9e29e3c
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-10-14 16:05:57 -0600

    Implemented getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -70,7 +70,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT properties.*, reservations.*, AVG(rating) AS average_rating
+  FROM reservations JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = $1 AND end_date < now()::date
+  GROUP BY reservations.id, properties.id
+  ORDER BY start_date
+  LIMIT $2;
+  `
+  const values = [guest_id, limit]
+  
+  return pool.query(queryString, values).then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
